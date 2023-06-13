a1aef0b08a7d9450e43f5679136739958197c1f0
Bjørnar Luteberget <Bjornar.Luteberget@sintef.no>
2023-06-13 08:52:37 +0200

    updated results script

--- mk_table.py
+++ mk_table.py
@@ -14,10 +14,15 @@ def tr_name(x):
 
 comparison = {}
 
+
+alg_time = {"bigm": {"total_time": 0.0, "alg_time": 0.0},
+            "satddd": {"total_time": 0.0, "alg_time": 0.0},
+            "mipddd": {"total_time": 0.0, "alg_time": 0.0}}
+
 for filename in [
-        "results/2023-05-23-continuous.json",
-        "results/2023-05-23-infsteps180.json",
-        "results/2023-05-23-finsteps123.json",
+        "results/2023-06-11-continuous.json",
+        "results/2023-06-11-infsteps180.json",
+        "results/2023-06-11-finsteps123.json",
         ]:
 
     print(f"# {filename}")
@@ -28,6 +33,7 @@ for filename in [
 
         if not instance["name"] in comparison:
             comparison[instance["name"]] = []
+
 
 
         worsttime = max([s["sol_time"] if "sol_time" in s else 9999 for s in instance["solves"]])
@@ -43,6 +49,12 @@ for filename in [
             comparison[instance["name"]].append((filename,"mipddd", mipddd["sol_time"] if "sol_time" in mipddd else None, f"{mipddd['sol_time']:.0f}" if "sol_time" in mipddd else "\\timeout"))
         #comparison.append((instance["name"],cmpx))
 
+        for solver_name,solver in [("bigm",bigm), ("satddd",satddd), ("mipddd", mipddd)]:
+            if "total_time" in solver:
+                alg_time[solver_name]["total_time"] += solver["total_time"]
+            if "algorithm_time" in solver:
+                alg_time[solver_name]["alg_time"] += solver["algorithm_time"]
+
         #if worsttime < 100.0:
         #    continue
         cols = [
@@ -55,17 +67,20 @@ for filename in [
                 f"{bigm['sol_time']:.0f}" if "sol_time" in bigm else "\\timeout",
 
 
-                # MIP DDD solve time
-                f"{mipddd['sol_time']:.0f}" if "sol_time" in mipddd else "\\timeout",
-
                 # Iterations, total intervals, avg. intervals (per event), solve time
                 str(satddd["iterations"]) if "iterations" in satddd else "-",
                 str(satddd["num_time_points"]) if "num_time_points" in satddd else "-",
-                f"{satddd['avg_time_points']:.1f}" if "avg_time_points" in satddd else "-",
                 f"{satddd['sol_time']:.0f}" if "sol_time" in satddd else "\\timeout",
+
+                # MIP DDD solve time
+                f"{mipddd['iteration']:.0f}" if "iteration" in mipddd else "-",
+                f"{mipddd['intervals']:.0f}" if "intervals" in mipddd else "-",
+                f"{mipddd['sol_time']:.0f}" if "sol_time" in mipddd else "\\timeout",
+
 
                 # Ratio of solve time between bigm and satddd
                 f"{(bigm['sol_time']/satddd['sol_time']):.1f}x" if "sol_time" in bigm and "sol_time" in satddd else "-",
+                f"{(mipddd['sol_time']/satddd['sol_time']):.1f}x" if "sol_time" in mipddd and "sol_time" in satddd else "-",
                 ]
 
         print(" & ".join(cols) + " \\\\")
@@ -73,6 +88,7 @@ for filename in [
 instances = [(k,v) for k,v in comparison.items()]
 instances.sort(key= lambda x: sum((-(t or 100000)  for _,_,t,_ in x[1] )))
 instances = instances[:10]
+
 
 print("#  objective comparison")
 for (n,xs) in instances:
@@ -82,3 +98,6 @@ for (n,xs) in instances:
         cols.append(t)
     print(" & ".join(cols) + " \\\\")
 
+
+print("# total time")
+print(alg_time)
