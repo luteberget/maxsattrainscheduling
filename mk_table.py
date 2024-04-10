ab9eeebe1608ca9a247fae2f8b346337eb012a6c
Bjørnar Luteberget <Bjornar.Luteberget@sintef.no>
2024-04-10 13:12:04 +0200

    new results for paper

--- mk_table.py
+++ mk_table.py
@@ -14,15 +14,29 @@ def tr_name(x):
 
 comparison = {}
 
+worst_instances = [
+    "stationA1",
+    "stationA2",
+    "stationA8",
+    "stationA11",
+    "stationA12",
+    "trackA1",
+    "trackA2",
+    "trackA8",
+    "trackA11",
+    "trackA12"]
+
+worst_instance_lines = []
+
 
 alg_time = {"bigm": {"total_time": 0.0, "alg_time": 0.0},
             "satddd": {"total_time": 0.0, "alg_time": 0.0},
             "mipddd": {"total_time": 0.0, "alg_time": 0.0}}
 
 for filename in [
-        "results/2024-04-09-cont.json",
-        "results/2024-04-09-infsteps180.json",
-        "results/2024-04-09-finsteps123.json",
+        "results/2024-04-10-cont.json",
+        "results/2024-04-10-infsteps180.json",
+        "results/2024-04-10-finsteps123.json",
         ]:
 
     print(f"# {filename}")
@@ -71,33 +85,62 @@ for filename in [
         cols = [
                 # Instance name
                 tr_name(instance["name"]),
+                ]
 
                 # BIGM iterations, conflicts, solve time
-                str(bigm["iteration"]) if "iteration" in bigm else "-",
-                str(bigm["added_conflict_pairs"]) if "added_conflict_pairs" in bigm else "-",
-                f"{bigm['sol_time']:.0f}" if "sol_time" in bigm else "\\timeout",
-                f"{bigm_gap:.0f}\\%",
+        bigm_to = "sol_time" not in bigm
 
+        def gapcols(g):
+            cols.append("-")
+            cols.append("-")
+            cols.append(f"\\multicolumn{{1}}{{c|}}{{\\scriptsize [{g:.0f}\\%]}}")
 
-                # Iterations, total intervals, avg. intervals (per event), solve time
-                str(satddd["iterations"]) if "iterations" in satddd else "-",
-                str(satddd["num_time_points"]) if "num_time_points" in satddd else "-",
-                f"{satddd['sol_time']:.0f}" if "sol_time" in satddd else "\\timeout",
-                f"{sat_gap:.0f}\\%",
+        if bigm_to:
+            gapcols(bigm_gap)
+        else:
+                cols += [
+                     str(bigm["iteration"]) if "iteration" in bigm else "-",
+                    str(bigm["added_conflict_pairs"]) if "added_conflict_pairs" in bigm else "-",
+                f"{bigm['sol_time']:.0f}" if "sol_time" in bigm else "\\timeout"]
+
+        sat_to = "sol_time" not in satddd
+        if sat_to:
+            gapcols(sat_gap)
+        else:
+            cols += [ # Iterations, total intervals, avg. intervals (per event), solve time
+                    str(satddd["iterations"]) if "iterations" in satddd else "-",
+                    str(satddd["num_time_points"]) if "num_time_points" in satddd else "-",
+                    f"{satddd['sol_time']:.0f}" if "sol_time" in satddd else "\\timeout",
+                    ]
 
                 # MIP DDD solve time
-                f"{mipddd['iteration']:.0f}" if "iteration" in mipddd else "-",
-                f"{mipddd['intervals']:.0f}" if "intervals" in mipddd else "-",
-                f"{mipddd['sol_time']:.0f}" if "sol_time" in mipddd else "\\timeout",
-                f"{mipddd_gap:.0f}\\%",
+        mipddd_to = "sol_time" not in mipddd
+        if mipddd_to:
+            gapcols(mipddd_gap)
+        else:
+            cols += [f"{mipddd['iteration']:.0f}" if "iteration" in mipddd else "-",
+                    f"{mipddd['intervals']:.0f}" if "intervals" in mipddd else "-",
+                    f"{mipddd['sol_time']:.0f}" if "sol_time" in mipddd else "\\timeout"]
 
 
                 # Ratio of solve time between bigm and satddd
-                f"{(bigm['sol_time']/satddd['sol_time']):.1f}x" if "sol_time" in bigm and "sol_time" in satddd else "-",
+        cols += [f"{(bigm['sol_time']/satddd['sol_time']):.1f}x" if "sol_time" in bigm and "sol_time" in satddd else "-",
                 f"{(mipddd['sol_time']/satddd['sol_time']):.1f}x" if "sol_time" in mipddd and "sol_time" in satddd else "-",
                 ]
 
         print(" & ".join(cols) + " \\\\")
+
+        if instance["name"] in worst_instances and "finsteps" in filename:
+            ws = [tr_name(instance["name"])]
+            for slv in [bigm, mipddd, satddd]:
+                ws.append( f"{slv['sol_time']:.0f}" if 'sol_time' in slv else "\\timeout ")
+
+            for slv in [bigm,mipddd]:
+                if 'sol_time' not in slv:
+                    ws.append("-")
+                else:
+                    ws.append(f"{(slv['sol_time']/satddd['sol_time']):.1f}x")
+            worst_instance_lines.append(" & ".join(ws) + " \\\\ ")
 
 instances = [(k,v) for k,v in comparison.items()]
 instances.sort(key= lambda x: sum((-(t or 100000)  for _,_,t,_ in x[1] )))
@@ -115,3 +158,7 @@ for (n,xs) in instances:
 
 print("# total time")
 print(alg_time)
+
+print("# worst instances")
+for l in worst_instance_lines:
+    print(l)
